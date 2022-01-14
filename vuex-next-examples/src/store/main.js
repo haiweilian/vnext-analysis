4045e66dc9668da248f2132ff0c9db42762aee47
haiweilian <haiweilian@foxmail.com>
2022-01-14 17:33:35 +0800

    vuex-next

---
+++ vuex-next-examples/src/store/main.js
@@ -0,0 +1,41 @@
+import { createStore } from "../../../vuex-next-4.0.0/dist/vuex.esm-bundler";
+
+const state = {
+  count: 0,
+};
+
+const mutations = {
+  increment(state) {
+    state.count++;
+  },
+  decrement(state) {
+    state.count--;
+  },
+};
+
+const actions = {
+  incrementIfOdd({ commit, state }) {
+    if ((state.count + 1) % 2 === 0) {
+      commit("increment");
+    }
+  },
+  incrementAsync({ commit }) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        commit("increment");
+        resolve();
+      }, 1000);
+    });
+  },
+};
+
+const getters = {
+  evenOrOdd: (state) => (state.count % 2 === 0 ? "even" : "odd"),
+};
+
+export default createStore({
+  state,
+  getters,
+  actions,
+  mutations,
+});
